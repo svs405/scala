0a006c2430eefc17081b4c4460bb09a9892662e5
vitaly.s <svs350531@gmail.com>
2016-12-22 22:00:03 -0500

    delete comments

--- scala2.scala
+++ scala2.scala
@@ -1,36 +1,9 @@
 object main {
 
-  
-  
   def isSorted[A](x:Array[A],compareFunction:(A,A)=>Boolean):Boolean={
-    
-    /* without using pattern matching
-     * 
-    def isEq(i:Int):Boolean={
-      if (i < x.length - 1) {
-       if( compareFunction( x(i), x(i+1) ) ) isEq(i+1)
-       else false
-      }
-      else true
-    }
-    */
-    
-    //with using pattern matching
     def isEq_r(i:Int):Boolean={
-      // если не использовать библиотечные функции, то как узнать конец массива???
       (i < x.length -1) match {
-        /* если писать x.length в кейсы вылетит ошибка мол это ненадежно
-         * но если не обращать на это внимание то все очень просто:
-         * i - наш итератор, которым мы ищем конец массива 
-         * но пустой массив така€ конструкци€ будет видеть как отсортированный
-         */
         case true => {
-          /*
-           * в функцию сравнени€ мы передаем i-й элемент и 
-           * следующий сразу за ним
-           * если функци€ сравнени€ сказала "хорошо" идем дальше по массиву
-           * рекурсивно вызыва€ isEq с индексом следующей пары дл€ сравнени€
-           */
           compareFunction( x(i), x(i+1) ) match{
             case true => isEq_r(i+1)
             case false => false
